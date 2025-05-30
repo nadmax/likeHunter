45e37cc73e061c2a7b741177b4f2c953dbf1c973
Maximilien Nadji <maximilien.nadji@pm.me>
2025-05-30 18:15:25 +0200

    add ping function call (#14)

--- src/handlers/interactionHandler.ts
+++ src/handlers/interactionHandler.ts
@@ -2,6 +2,7 @@ import { client } from '../utils/client';
 import * as holidaysCmd from '../commands/holidays';
 import * as scanCmd from '../commands/scan';
 import * as welcomeCmd from '../commands/welcome';
+import * as pingCmd from '../commands/ping';
 
 client.on('interactionCreate', async interaction => {
     if (interaction.isButton()) {
@@ -15,7 +16,8 @@ client.on('interactionCreate', async interaction => {
 
     const handlers: Record<string, (interaction: any) => void> = {
         vacances: holidaysCmd.execute,
-        scan: scanCmd.execute
+        scan: scanCmd.execute,
+        ping: pingCmd.execute
     };
     const handler = handlers[interaction.commandName];
     if (handler) {
