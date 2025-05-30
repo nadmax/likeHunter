f868b1685948a37acf9cbd1c0b9a28d06293a789
Maximilien Nadji <maximilien.nadji@pm.me>
2025-05-30 18:00:12 +0200

    fix docker volume path (#13)

--- src/utils/holidays.ts
+++ src/utils/holidays.ts
@@ -1,10 +1,14 @@
 import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'fs';
 import { dirname } from 'path';
 
-const HOLIDAYS_FILE = './data/vacances.json';
+const HOLIDAYS_FILE = process.env.HOLIDAYS_FILE_PATH!;
 const dir = dirname(HOLIDAYS_FILE);
 if (!existsSync(dir)) {
+    console.log("Data folder does not exist, creating...");
     mkdirSync(dir, { recursive: true });
+
+    console.log("Holidays file does not exist, creating...");
+    writeFileSync(HOLIDAYS_FILE, '[]', 'utf-8');
 }
 
 export function getHolidaysList(): string[] {
